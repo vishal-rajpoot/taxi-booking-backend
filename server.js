f197c8216adadf0bbdb8c37699031183260dd81d
psahir <psahir289@gmail.com>
2025-08-29 16:04:19 +0400

    fix: fixed server working communication

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = createServer(app);
 
 initializeSocket(server);
 
-const PORT = config?.port || 8090;
+const PORT = config?.port || 8000;
 
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
